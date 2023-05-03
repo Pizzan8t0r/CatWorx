0651e15f156d0add1d03a05ff9b9d16cd1ca01e7
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-02 18:17:15 -0700

    using a for loop to write the employees names on the list

--- Program.cs
+++ Program.cs
@@ -10,8 +10,14 @@ namespace CatWorx.BadgeMaker
         List<string> employees = new List<string>() { "adam", "amy" };
         employees.Add("barbara");
         employees.Add("billy");
-        Console.WriteLine("My employees include {0}, {1}, {2}, {3}", employees[0], employees[1], employees[2], employees[3]);
+        for (int i = 0; i < employees.Count; i++)
+        {
+        Console.WriteLine(employees[i]);
         }
+        } 
     }
 }
-// My employees include adam, amy, barbara, billy
+// adam
+// amy
+// barbara
+// billy
