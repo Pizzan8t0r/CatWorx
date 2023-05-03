de0dbe046d3219f4eb3037dc22ee909e60630d10
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-02 18:10:26 -0700

    adding employees to the list

--- Program.cs
+++ Program.cs
@@ -6,13 +6,12 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-          string[] favFoods = new string[3] {"pizza", "doughnuts", "icecream"};
-          string firstFood = favFoods[0];
-          string secondFood = favFoods[1];
-          string thirdFood = favFoods[2];
-          Console.WriteLine("I like {0}, {1}, and {2}", firstFood, secondFood, thirdFood);
-   
+         // this bad boy declares a list and its data types using the angle bracket notation, similar to the dictionary
+        List<string> employees = new List<string>() { "adam", "amy" };
+        employees.Add("barbara");
+        employees.Add("billy");
+        Console.WriteLine("My employees include {0}, {1}, {2}, {3}", employees[0], employees[1], employees[2], employees[3]);
         }
     }
 }
-// I like pizza, doughnuts, and icecream
+// My employees include adam, amy, barbara, billy
