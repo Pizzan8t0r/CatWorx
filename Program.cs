f977eb9ceb06848f70bdd3d80b9b82d11b5adc3b
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-03 16:28:30 -0700

    updated the employee list program

--- Program.cs
+++ Program.cs
@@ -3,37 +3,41 @@
 namespace CatWorx.BadgeMaker
 {
     class Program
-    {
-      static void Main(string[] args)
 {
-  // This is our employee-getting code now
-  List<string> employees = GetEmployees();
-
-  for (int i = 0; i < employees.Count; i++) 
+  static List<string> GetEmployees()
   {
-    Console.WriteLine(employees[i]);
-  }
-}
-
-static List<string> GetEmployees()
-{
-  List<string> employees = new List<string>();
-  while (true) 
-  {
-    Console.WriteLine("Please enter a name: (leave empty to exit): ");
-    string input = Console.ReadLine() ?? "";
-    if (input == "") 
+    List<string> employees = new List<string>();
+    while (true)
     {
-      break;
+
+      Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+      string input = Console.ReadLine() ?? "";
+
+      if (input == "")
+      {
+        break;
+      }
+      employees.Add(input);
     }
-      
-    employees.Add(input);
+    return employees;
   }
-  // This is important!
-  return employees;
-}
+
+  static void PrintEmployees(List<string> employees)
+  {
+    for (int i = 0; i < employees.Count; i++)
+    {
+      Console.WriteLine(employees[i]);
+    }
   }
- } 
-}
-// The left-hand side of the statement assigns a List<string> to employees. The right-hand side calls the new method, which returns a List<string>. 
-// This is very similar to the original employees declaration that we moved into GetEmployees()
+
+  static void Main(string[] args)
+  {
+    List<string> employees = GetEmployees();
+    PrintEmployees(employees);
+   }
+  }
+ }
+ 
+// GetEmployees() had a return type but no parameters. 
+// PrintEmployees() will require a parameter but won't have a return type.
