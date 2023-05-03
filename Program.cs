598dd0e6f356a72fa6128776443fd46ac5630f63
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-02 17:58:55 -0700

    some baseballllll

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
+     
 namespace CatWorx.BadgeMaker
 {
     class Program
     {
         static void Main(string[] args)
         {
-            bool isCold = true;
-            Console.WriteLine(isCold ? "drink" : "add ice");
-            Console.WriteLine(!isCold ? "drink" : "add ice");
-
-
+           Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+    { "firstInning", 10 },
+    { "secondInning", 20},
+    { "thirdInning", 30},
+    { "fourthInning", 40},
+    { "fifthInning", 50}
+};
+ Console.WriteLine("----------------");
+Console.WriteLine("  Scoreboard");
+Console.WriteLine("----------------");
+Console.WriteLine("Inning |  Score");
+Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
+Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
+Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
+Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
+Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+   
         }
     }
 }
 
-// drnk
-// add ice
+----------------
+  Scoreboard
+----------------
+Inning |  Score
+   1   |    10
+   2   |    20
+   3   |    30
+   4   |    40
+   5   |    50
