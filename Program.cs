c619aa8f46410f29a4b53fc7d9e6ed1f9db3d9fe
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-02 17:24:52 -0700

    more math stuff

--- Program.cs
+++ Program.cs
@@ -6,16 +6,36 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-          double side = 10;
-          double area = side * side;
-          Console.WriteLine("area: {0}", area);
+            Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
+
+Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
+
+Console.WriteLine(1 + 2 * 3);     // order of operations
+
+Console.WriteLine(10 / 3.0);      // int's and doubles
+
+Console.WriteLine(10 / 3);        // int's 
+
+Console.WriteLine("12" + "3");    // What happens here?
+
+int num = 10;
+
+num += 100;
+
+Console.WriteLine(num);
+
+num ++;
+
+Console.WriteLine(num);
         }
     }
 }
 
-
-
-//float side = 4;
-     //     float area = side * side;
-       //   Console.WriteLine("area : {0}", area);
-        // -> 16
+// 6
+// 1
+// 7
+// 3.3333333333333335
+// 3
+// 123
+// 110
+// 111
