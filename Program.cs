46430ff6eff10a5250d768afa6dc2a837389d1cd
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-03 17:13:22 -0700

    more information on the get employees method as well as the print employee method

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace CatWorx.BadgeMaker
 {
     class Program
 {
+  // get employee method
   static List<string> GetEmployees()
   {
     List<string> employees = new List<string>();
@@ -22,7 +23,8 @@ namespace CatWorx.BadgeMaker
     }
     return employees;
   }
-
+  // if a static method wants to call another method inside the same class directly, that method must also be static
+// print employee method
   static void PrintEmployees(List<string> employees)
   {
     for (int i = 0; i < employees.Count; i++)
@@ -41,3 +43,7 @@ namespace CatWorx.BadgeMaker
  
 // GetEmployees() had a return type but no parameters. 
 // PrintEmployees() will require a parameter but won't have a return type.
+// Because we'll hand off the list of employee names to the caller of the method, we must make sure to return it at the end of the method. 
+// This fulfills the contract we created in the method signature, which promises to return a list of strings
+// C# provides a dictionary structure, which can store keys and values similar to Javascript objects. This might seem like a candidate for storing the employee data  
+// Dictionaries can only store values of the same type
